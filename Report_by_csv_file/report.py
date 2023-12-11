84b2cd689729244748072566477ec9d55542f50b
vladimir_tsodikov <vovatsodikov@gmail.com>
2023-12-11 22:26:52 +0300

    minor changes

--- Report_by_csv_file/report.py
+++ Report_by_csv_file/report.py
@@ -1,5 +1,5 @@
-from sys import exit
-#  only to exit the program from the menu
+from sys import exit  # to exit the program from the menu
+from collections import defaultdict  # for convenient initialization
 
 
 def print_menu() -> None:
@@ -24,16 +24,17 @@ def choice():
     пользователь сам не закроет программу (нажатием цифры 4).
     """
     choose = int(input())
-    if choose == 1:
-        dep_and_teams()
-    elif choose == 2:
-        print_report_to_screen(create_report())
-    elif choose == 3:
-        print_report_to_file(create_report())
-    elif choose == 4:
-        exit()
-    else:
-        print('Неверный ввод. Повторите попытку.\n')
+    match choose:
+        case 1:
+            dep_and_teams()
+        case 2:
+            print_report_to_screen(create_report())
+        case 3:
+            print_report_to_file(create_report())
+        case 4:
+            exit()
+        case _:
+            print('Неверный ввод. Повторите попытку.\n')
     print_menu()
     choice()
 
@@ -47,17 +48,14 @@ def dep_and_teams() -> dict | None:
     Возвращает ничего или словарь, где ключи - названия департаментов,
     а значения - списки из относящихся к данному департаменту команд
     """
-    deps = {}
+    deps = defaultdict(set)
     EXAMPLE = 'Департамент'
     with open('Corp_Summary.csv', 'r', encoding='utf-8') as table:
         for line in table:
             line = line.strip().split(';')
             cur_dep, cur_team = line[1], line[2]
-            if cur_dep != EXAMPLE:
-                if cur_dep not in deps:
-                    deps[cur_dep] = [cur_team]
-                elif cur_team not in deps[cur_dep]:
-                    deps[cur_dep].append(cur_team)
+            if cur_dep != EXAMPLE:  # чтобы игнорировать шапку таблицы
+                deps[cur_dep].add(cur_team)
     for dep, team in deps.items():
         print(f' {dep}: ', end='\n')
         for cur_team in team:
@@ -87,17 +85,22 @@ def create_report() -> dict:
             if cur_dep != EXAMPLE:
                 cur_wage = int(line[5])
                 if cur_dep not in deps:
-                    deps[cur_dep] = [1, cur_wage, cur_wage, cur_wage]
+                    deps[cur_dep] = {'num_employees': 1,
+                                     'min_salary': cur_wage,
+                                     'max_salary': cur_wage,
+                                     'total_salary': cur_wage
+                                     }
                     #  численность, мин. зп, макс. зп, общая зп на весь департ.
                 else:
-                    deps[cur_dep][0] += 1
-                    if cur_wage < deps[cur_dep][1]:
-                        deps[cur_dep][1] = cur_wage
-                    if cur_wage > deps[cur_dep][2]:
-                        deps[cur_dep][2] = cur_wage
-                    deps[cur_dep][3] += cur_wage
+                    deps[cur_dep]['num_employees'] += 1
+                    if cur_wage < deps[cur_dep]['min_salary']:
+                        deps[cur_dep]['min_salary'] = cur_wage
+                    if cur_wage > deps[cur_dep]['max_salary']:
+                        deps[cur_dep]['max_salary'] = cur_wage
+                    deps[cur_dep]['total_salary'] += cur_wage
     for dep_values in deps.values():
-        dep_values.append(round(dep_values[3]/dep_values[0], 2))
+        dep_values['average_salary'] = round(dep_values['total_salary'] /
+                                             dep_values['num_employees'], 2)
         #  добавляем информацию о средней зп в каждом департаменте
     return deps
 
@@ -109,11 +112,11 @@ def print_report_to_screen(deps: dict) -> None:
     """
     for dep, data in deps.items():
         print(f'Департамент: {dep}\n'
-              f'Численность: {data[0]}\n'
-              f'Минимальная зарплата: {data[1]}\n'
-              f'Максимальная зарплата: {data[2]}\n'
-              f'Общая зарплата, выделяемая на департамент: {data[3]}\n'
-              f'Средняя зарплата: {data[4]}\n\n'
+              f'Численность: {data['num_employees']}\n'
+              f'Минимальная зарплата: {data['min_salary']}\n'
+              f'Максимальная зарплата: {data['max_salary']}\n'
+              f'Общая зарплата, выделяемая на департамент: {data['total_salary']}\n'
+              f'Средняя зарплата: {data['average_salary']}\n\n'
               )
 
 
@@ -125,12 +128,12 @@ def print_report_to_file(deps: dict, output: str = 'output.csv') -> None:
     with open(output, 'w', encoding='utf-16') as out_file:
         for dep, data in deps.items():
             out_file.write(
-                f'Департамент; {dep}\n'
-                f'Численность; {data[0]}\n'
-                f'Минимальная зарплата; {data[1]}\n'
-                f'Максимальная зарплата; {data[2]}\n'
-                f'Общая зарплата, выделяемая на департамент; {data[3]}\n'
-                f'Средняя зарплата: {data[4]}\n\n'
+                f'Департамент: {dep}\n'
+                f'Численность: {data['num_employees']}\n'
+                f'Минимальная зарплата: {data['min_salary']}\n'
+                f'Максимальная зарплата: {data['max_salary']}\n'
+                f'Общая зарплата, выделяемая на департамент: {data['total_salary']}\n'
+                f'Средняя зарплата: {data['average_salary']}\n\n'
                 )
 
 
