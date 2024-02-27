20533a94c971e263eaa17a059c800673e97cb1a2
vladimir_tsodikov <vovatsodikov@gmail.com>
2024-02-27 15:17:46 +0300

    Final changes

--- Tic_tac_toe_game_Bot/tic_tac_toe_game.py
+++ Tic_tac_toe_game_Bot/tic_tac_toe_game.py
@@ -5,7 +5,7 @@
 import datetime
 import logging
 import os
-from copy import deepcopy
+# from copy import deepcopy
 
 import random
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
@@ -19,16 +19,16 @@ from telegram.ext import (
 
 TOKEN = os.getenv("TG_TOKEN")
 
-SIZE = 3
+# SIZE = 4
 
-CONTINUE_GAME, FINISH_GAME = range(2)
+WAITING_FOR_SIZE, CONTINUE_GAME, FINISH_GAME = range(3)
 
 FREE_SPACE = "."
 CROSS = "❎"
 ZERO = "⭕"
 
 
-DEFAULT_STATE = [[FREE_SPACE for _ in range(SIZE)] for _ in range(SIZE)]
+# DEFAULT_STATE = [[FREE_SPACE for _ in range(SIZE)] for _ in range(SIZE)]
 
 
 def get_fname():
@@ -36,43 +36,71 @@ def get_fname():
     return datetime.datetime.now().strftime("%d_%m_%Y__%H_%M_%S")
 
 
-def get_default_state():
+def get_default_state(size):
     """Вспомогательная функция для копирования исходного поля"""
-    return deepcopy(DEFAULT_STATE)
+    return [[FREE_SPACE for _ in range(size)] for _ in range(size)]
 
 
-def generate_keyboard(state: list[list[str]]) -> list[list[InlineKeyboardButton]]:
+def generate_keyboard(state: list[list[str]], size: int) -> list[list[InlineKeyboardButton]]:
     """Генерирует клавиатуру для игры в крестики-нолики размером SIZExSIZE (telegram buttons)"""
     return [
         [
             InlineKeyboardButton(state[r][c], callback_data=f"{r}{c}")
-            for c in range(SIZE)
+            for c in range(size)
         ]
-        for r in range(SIZE)
+        for r in range(size)
     ]
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Send message on `/start`."""
-    context.user_data["keyboard_state"] = get_default_state()
-    keyboard = generate_keyboard(context.user_data["keyboard_state"])
+    keyboard = [
+        [
+            InlineKeyboardButton('3', callback_data='3'),
+            InlineKeyboardButton('4', callback_data='4')
+        ],
+        [
+            InlineKeyboardButton('5', callback_data='5'),
+            InlineKeyboardButton('6', callback_data='6')
+        ]
+    ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text(
         f"{update.message.from_user.first_name}, добро пожаловать в игру! "
+        "Выберите размер игрового поля:",
+        reply_markup=reply_markup,
+    )
+
+    return WAITING_FOR_SIZE
+
+
+async def start_game(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    print('start_game_start')
+    query = update.callback_query
+    size = int(query.data)
+    context.user_data["size"] = size
+    print(context.user_data["size"])
+    context.user_data["keyboard_state"] = get_default_state(size)
+    keyboard = generate_keyboard(context.user_data["keyboard_state"], size)
+    # print(keyboard)
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await query.edit_message_text(
         "Ваш ход! Поставьте X на любое свободное место",
         reply_markup=reply_markup,
     )
     # print(update.message.from_user.first_name)
     # print(context.user_data)
+    print('start_game_end')
     return CONTINUE_GAME
 
 
 async def player_move(update, context, query) -> int:
     """Function for handling inline keyboard presses"""
     keyboard = context.user_data["keyboard_state"]
-    # print(keyboard)
+    print(keyboard)
     # print(update.callback_query.message)
     r, c = map(int, query.data)
+    print('player_move: ', r, c)
     if keyboard[r][c] != FREE_SPACE:
         # если тыкает в занятую клетку не первый раз подряд - больше ничего не меняем
         if "в занятую ячейку" in update.callback_query.message.text:
@@ -89,6 +117,8 @@ async def player_move(update, context, query) -> int:
     new_val = CROSS
     keyboard[r][c] = new_val
     context.user_data["keyboard_state"] = keyboard
+    print(context.user_data["keyboard_state"])
+    print('end player move')
     return CONTINUE_GAME
 
 
@@ -100,6 +130,7 @@ async def game_over(update, context, who: str) -> int:
 
 async def check_end(update, context) -> int:
     """Проверка, произошёл ли выигрыш/проигрыш/ничья"""
+    print('1 check')
     if won(context.user_data["keyboard_state"], CROSS):
         # выиграл игрок
         logging.info("player won")
@@ -114,6 +145,7 @@ async def check_end(update, context) -> int:
         else:
             logging.info("happy won")
             return await game_over(update, context, "happy")
+    print('2 check')
     return CONTINUE_GAME
 
 
@@ -121,36 +153,46 @@ async def continue_or_end(update, context, query) -> int:
     """Если произошёл выигрыш/проигрыш/ничья, игра завершается.
     Иначе предыдущее сообщение удаляется и игра продолжается.
     """
-    if await check_end(update, context):
+    print('1 cont or end')
+    if not await check_end(update, context):
         return FINISH_GAME
-
+    print('1.5 cont or end')
     # удаление предыдущего сообщения
     await context.bot.delete_message(
         chat_id=update.callback_query.message.chat.id,
         message_id=update.callback_query.message.id,
     )
 
-    keyboard = generate_keyboard(context.user_data["keyboard_state"])
+    size = context.user_data["size"]
+    keyboard = generate_keyboard(context.user_data["keyboard_state"], size)
     reply_markup = InlineKeyboardMarkup(keyboard)
+    print('2 cont_or_end')
     await context.application.bot.sendMessage(
         text=f"{query.from_user.first_name}, Ваш ход! "
         "Поставьте X на любое свободное место",
         chat_id=update.callback_query.message.chat.id,
         reply_markup=reply_markup,
     )
+    print('end cont or end')
     return CONTINUE_GAME
 
 
 async def game(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Main processing of the game"""
+    print('game!')
+    # size = context.user_data["size"]
     query = update.callback_query
+    # print(query)
     if await query.answer():
-        if await player_move(update, context, query):
+        print('query сработал ')
+        if await player_move(update, context, query) != CONTINUE_GAME:
+            print('ход сработал')
             return CONTINUE_GAME
 
         return await continue_or_end(update, context, query)
     else:
         logging.info("Problems")
+    print('конец хода')
     return CONTINUE_GAME
 
 
@@ -186,7 +228,7 @@ def won_by_side_diag(fields: list[list[str]], who: str) -> bool:
     "Проверка, есть ли выигрышная комбинация на побочной диагонали"
     win = True
     for i in range(len(fields)):
-        if fields[i][SIZE-1-i] != who:
+        if fields[i][-1-i] != who:
             win = False
             break
     return win
@@ -233,7 +275,7 @@ async def before_end(update: Update, context: ContextTypes.DEFAULT_TYPE, who: st
     снизу отправляется ещё одно сообщение с результатом партии
     и приглашением сыграть снова
     """
-    keyboard = generate_keyboard(context.user_data["keyboard_state"])
+    keyboard = generate_keyboard(context.user_data["keyboard_state"], context.user_data["size"])
     reply_markup = InlineKeyboardMarkup(keyboard)
     play_again = "Чтобы начать новую игру, нажмите /start"
     await context.bot.edit_message_reply_markup(
@@ -263,6 +305,9 @@ async def end(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     return ConversationHandler.END
 
 
+# def get_size(context: ContextTypes.DEFAULT_TYPE) -> int:
+#     return context.user_data["size"]
+
 def main() -> None:
     """Run the bot"""
 
@@ -286,15 +331,21 @@ def main() -> None:
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
         states={
+            WAITING_FOR_SIZE: [
+                CallbackQueryHandler(start_game, pattern="^" + f"{i}" + "$")
+                for i in range(3, 7)
+            ],
             CONTINUE_GAME: [
-                CallbackQueryHandler(game, pattern="^" + f"{r}{c}" + "$")
-                for r in range(SIZE)
-                for c in range(SIZE)
+                CallbackQueryHandler(game)
+                # , pattern="^" + f"{r}{c}" + "$")
+                # for r in range(get_size(ContextTypes.DEFAULT_TYPE))
+                # for c in range(get_size(ContextTypes.DEFAULT_TYPE))
             ],
             FINISH_GAME: [
-                CallbackQueryHandler(end, pattern="^" + f"{r}{c}" + "$")
-                for r in range(SIZE)
-                for c in range(SIZE)
+                CallbackQueryHandler(end)
+                # , pattern="^" + f"{r}{c}" + "$")
+                # for r in range(get_size(ContextTypes.DEFAULT_TYPE))
+                # for c in range(get_size(ContextTypes.DEFAULT_TYPE))
             ],
         },
         fallbacks=[CommandHandler("start", start)],
